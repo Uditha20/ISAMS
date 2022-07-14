e355946d0f8d1f2eb2824602e72e38c628aba4c9
Uditha20 <udithaindunil5@gmail.com>
2022-07-15 06:43:10 +0800

    old

--- admin_login.php
+++ admin_login.php
@@ -17,6 +17,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             
             echo '<script> alert("invalid login") </script>';
             include("login.php");
+            echo "uditha";
         }
 
        
